9f2233f82242cb10a0cec9d43c6de53ffccdc78c
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-06-13 22:11:24 -0400

    remove unused styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,31 +61,6 @@
 <!-- </main> -->
 
 <style>
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	:global(body) {
-		margin: 0;
-		padding: 0;
-	}
-
-	main {
-		padding: 2em 1em 1em 1em;
-		text-align: center;
-		animation: fade 1s;
-		margin: 0 auto;
-	}
-
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
 	.flex {
 		display: flex;
 	}
