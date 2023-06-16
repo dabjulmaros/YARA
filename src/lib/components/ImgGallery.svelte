c6141c1ec6de0eaac03eaa7bbcc6a154a7eade66
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-06-16 12:44:43 -0400

    polish buttons and added captions to gallery

--- src/lib/components/ImgGallery.svelte
+++ src/lib/components/ImgGallery.svelte
@@ -1,30 +1,48 @@
 <script>
-	import { createEventDispatcher } from 'svelte';
+	import { onMount, createEventDispatcher } from 'svelte';
+	import { getImgSrc } from '$lib/utils/getImgSrc';
 
-	export let srcArr;
+	export let expando;
+	let srcArr = [];
+	let capArr = [];
 
 	const dispatch = createEventDispatcher();
 
 	let index = 0;
 
 	function fullHeightImage(e) {
-		console.log(e);
 		dispatch('fullImgGall', {
 			target: e.target,
 		});
 	}
+	onMount(() => {
+		srcArr = getImgSrc(expando);
+		capArr = getCaptions(expando);
+	});
 
 	function changeImg(change) {
 		index += change;
 		if (index >= srcArr.length) index = 0;
 		else if (index < 0) index = srcArr.length - 1;
 	}
+	function getCaptions(expando) {
+		const returnArr = [];
+		let captions = expando.matchAll(/Caption: ([^<]+) </g);
+		for (const match of captions) {
+			returnArr.push(match[1]);
+		}
+		return returnArr;
+	}
 </script>
 
 <div class="gallery">
 	<button class="outline" on:click={() => changeImg(-1)}>◀</button>
 	<div class="imgHolder" style="position:relative;">
-		<mark style="position: absolute; z-index:1;opacity:.4;right:0;bottom:0" class="mark">
+		<mark
+			style="position: absolute; z-index:1;opacity:.4;right:0;bottom:0"
+			class="mark"
+			data-tooltip={capArr[index]}
+		>
 			{index + 1}/{srcArr.length}
 		</mark>
 		<div class="carousel" style={`transform:translate(-${index * 100}%)`}>
@@ -45,12 +63,15 @@
 
 <style>
 	button {
-		width: 3rem;
-		height: 3rem;
-		margin: 0;
-		padding: 0.8rem;
-		line-height: 0rem;
-		font-size: 1.5rem;
+		cursor: pointer;
+		margin: 1rem;
+		border: none;
+		height: 2.2rem;
+		width: 2.7rem;
+		z-index: 1;
+		line-height: 1rem;
+		font-size: 1.4rem;
+		padding: 0rem;
 	}
 	.gallery {
 		width: 100%;
@@ -69,7 +90,6 @@
 	}
 	.imgHolder {
 		width: calc(100% - 2px);
-		overflow: hidden;
 		margin: 0 auto;
 	}
 	.imgHolder div {
