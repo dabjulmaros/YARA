a1746ea524ad5ce9e53259d2ed23b668aab367e1
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-06-23 10:06:35 -0400

    points and time update

--- src/lib/utils/getPoints.js
+++ src/lib/utils/getPoints.js
@@ -1,4 +1,9 @@
-export function getPoints(points,score) {
-  if (points) return points;
-  return score;
+export function getPoints(points, score) {
+  if (points) return points +" points";
+  if (score > 1000000)
+    return (score / 1000000).toFixed(1) + "M points";
+  else if (score > 1000)
+    return (score / 1000).toFixed(1) + "k points";
+  else return score+" points";
+
 }
--- src/lib/utils/getTime.js
+++ src/lib/utils/getTime.js
@@ -1,14 +1,26 @@
-export function getTime(time,created) {
+export function getTime(time, created) {
   if (time) return time;
 
   // time code times 1000 as js dates are on miliseconds
   const date = new Date(created * 1000);
+  const now = new Date();
+  let diff = now - date;
+  diff /= (1000 * 60);
+  if (diff < 1)
+    return "Just Now"
 
-  return date.toLocaleString('en-us', {
-    year: 'numeric',
-    month: 'short',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  });
+  if (diff < 60)
+    return Math.round(diff) + ` minute${Math.round(diff)>1?'s':''} ago`;
+
+  diff /= 60
+  if (diff < 24)
+    return Math.round(diff) + ` hour${Math.round(diff)>1?'s':''} ago`;
+
+  diff /= 30.5
+  if (diff < 12)
+    return Math.round(diff) + ` month${Math.round(diff)>1?'s':''} ago`;
+
+  diff /= 365;
+  return Math.round(diff) + ` year${Math.round(diff)>1?'s':''} ago`;
+
 }
