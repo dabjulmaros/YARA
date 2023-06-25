97e6567b1167044f137cc29718b9e905cdd4f0af
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-06-24 23:34:16 -0400

    html bug fix

--- src/lib/utils/htmlDecode.js
+++ src/lib/utils/htmlDecode.js
@@ -3,5 +3,6 @@ export function htmlDecode(s) {
   const text = document.createElement("text");
   text.innerHTML = s;
   text.textContent=text.textContent.replaceAll('<a','<a target="_blank"');
+  text.textContent=text.textContent.replaceAll('src="//','src="https://');
   return text.textContent;
 }
