ae5445e854f2091940a77638f9209ed187bd9c42
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-08-01 22:20:12 -0400

    added open in broswer to context menu

--- src/electron.cjs
+++ src/electron.cjs
@@ -1,13 +1,13 @@
-const windowStateManager = require ('electron-window-state');
-const { app, BrowserWindow, shell } = require ('electron');
-const contextMenu = require ('electron-context-menu');
-const serve = require ('electron-serve');
-const path = require ('path');
+const windowStateManager = require('electron-window-state');
+const { app, BrowserWindow, shell } = require('electron');
+const contextMenu = require('electron-context-menu');
+const serve = require('electron-serve');
+const path = require('path');
 
 try {
-	require('electron-reloader')(module);
+  require('electron-reloader')(module);
 } catch (e) {
-	console.error(e);
+  console.error(e);
 }
 
 const serveURL = serve({ directory: '.' });
@@ -18,160 +18,167 @@ let over18 = false;
 let localStorage = {};
 
 function createWindow() {
-	let windowState = windowStateManager({
-		defaultWidth: 800,
-		defaultHeight: 600,
-	});
+  let windowState = windowStateManager({
+    defaultWidth: 800,
+    defaultHeight: 600,
+  });
 
-	const mainWindow = new BrowserWindow({
-		backgroundColor: 'whitesmoke',
-		titleBarStyle: 'hidden',
-		titleBarOverlay: {
-			color: '#11191f',
-			symbolColor: '#1095c1',
-			height: 30,
-		},
-		// autoHideMenuBar: true,
-		// trafficLightPosition: {
-		//   x: 17,
-		//   y: 32,
-		// },
-		minHeight: 450,
-		minWidth: 500,
-		webPreferences: {
+  const mainWindow = new BrowserWindow({
+    backgroundColor: 'whitesmoke',
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#11191f',
+      symbolColor: '#1095c1',
+      height: 30,
+    },
+    // autoHideMenuBar: true,
+    // trafficLightPosition: {
+    //   x: 17,
+    //   y: 32,
+    // },
+    minHeight: 450,
+    minWidth: 500,
+    webPreferences: {
       // webviewTag: true, //tab-groups
-			enableRemoteModule: true,
-			contextIsolation: true,
-			nodeIntegration: true,
-			spellcheck: false,
-			devTools: true,//dev,
-			preload: path.join(__dirname, 'preload.cjs'),
-		},
-		x: windowState.x,
-		y: windowState.y,
-		width: windowState.width,
-		height: windowState.height,
-	});
+      enableRemoteModule: true,
+      contextIsolation: true,
+      nodeIntegration: true,
+      spellcheck: false,
+      devTools: true,//dev,
+      preload: path.join(__dirname, 'preload.cjs'),
+    },
+    x: windowState.x,
+    y: windowState.y,
+    width: windowState.width,
+    height: windowState.height,
+  });
 
-	windowState.manage(mainWindow);
+  windowState.manage(mainWindow);
 
-	mainWindow.once('ready-to-show', async () => {
-		mainWindow.show();
-		mainWindow.focus();
+  mainWindow.once('ready-to-show', async () => {
+    mainWindow.show();
+    mainWindow.focus();
 
-		await mainWindow.webContents.executeJavaScript('({...localStorage});', true).then((ls) => {
-			localStorage = ls;
-		});
+    await mainWindow.webContents.executeJavaScript('({...localStorage});', true).then((ls) => {
+      localStorage = ls;
+    });
 
-		if (localStorage['over18']) over18 = JSON.parse(localStorage['over18']);
-		else
-			mainWindow.webContents.executeJavaScript(
-				`localStorage.setItem('over18', ${over18})`,
-				true,
-			);
-	});
+    if (localStorage['over18']) over18 = JSON.parse(localStorage['over18']);
+    else
+      mainWindow.webContents.executeJavaScript(
+        `localStorage.setItem('over18', ${over18})`,
+        true,
+      );
+  });
 
-	mainWindow.on('close', () => {
-		windowState.saveState(mainWindow);
-	});
+  mainWindow.on('close', () => {
+    windowState.saveState(mainWindow);
+  });
 
-	//cors
-	mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
-		const { requestHeaders } = details;
-		UpsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', ['*']);
-		callback({ requestHeaders });
-	});
+  //cors
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+    const { requestHeaders } = details;
+    UpsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', ['*']);
+    callback({ requestHeaders });
+  });
 
-	mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-		const { responseHeaders } = details;
-		UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', ['*']);
-		UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', ['*']);
-		callback({
-			responseHeaders,
-			...details.requestHeaders,
-		});
-	});
+  mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    const { responseHeaders } = details;
+    UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', ['*']);
+    UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', ['*']);
+    callback({
+      responseHeaders,
+      ...details.requestHeaders,
+    });
+  });
 
-	mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-		if (url.includes('localhost')) return { action: 'deny' };
-		shell.openExternal(url);
-		return { action: 'deny' };
-	});
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.includes('localhost')) return { action: 'deny' };
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
 
-	return mainWindow;
+  return mainWindow;
 }
 
 //https://github.com/sindresorhus/electron-context-menu
 contextMenu({
-	showSelectAll: false,
-	showLookUpSelection: true,
-	// showSearchWithGoogle: true,
-	showCopyImage: true,
-	showSaveImageAs: true,
-	showCopyLink: true,
-	prepend: (defaultActions, parameters, browserWindow) => [
-		{
-			label: 'Show Over 18 Content',
-			visible: over18 == false,
-			click: () => {
-				appOver18();
-			},
-		},
-		{
-			label: 'Hide Over 18 Content',
-			visible: over18 == true,
-			click: () => {
-				appOver18();
-			},
-		},
-	],
+  showSelectAll: false,
+  showLookUpSelection: true,
+  // showSearchWithGoogle: true,
+  showCopyImage: true,
+  showSaveImageAs: true,
+  showCopyLink: true,
+  prepend: (defaultActions, parameters, browserWindow) => [
+    {
+      label: "Open In Browser",
+      visible: parameters.linkURL !== "",
+      click: () => { shell.openExternal(parameters.linkURL) }
+    }
+  ],
+  append: () => [
+    {
+      label: 'Show Over 18 Content',
+      visible: over18 == false,
+      click: () => {
+        appOver18();
+      },
+    },
+    {
+      label: 'Hide Over 18 Content',
+      visible: over18 == true,
+      click: () => {
+        appOver18();
+      },
+    }
+  ]
 });
 
 function loadVite(port) {
-	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
-		console.log('Error loading URL, retrying', e);
-		setTimeout(() => {
-			loadVite(port);
-		}, 200);
-	});
+  mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
+    console.log('Error loading URL, retrying', e);
+    setTimeout(() => {
+      loadVite(port);
+    }, 200);
+  });
 }
 
 function createMainWindow() {
-	mainWindow = createWindow();
-	mainWindow.once('close', () => {
-		mainWindow = null;
-	});
+  mainWindow = createWindow();
+  mainWindow.once('close', () => {
+    mainWindow = null;
+  });
 
-	if (dev) loadVite(port);
-	else serveURL(mainWindow);
+  if (dev) loadVite(port);
+  else serveURL(mainWindow);
 }
 
 app.once('ready', createMainWindow);
 app.on('activate', () => {
-	if (!mainWindow) {
-		createMainWindow();
-	}
+  if (!mainWindow) {
+    createMainWindow();
+  }
 });
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 //cors work around https://pratikpc.medium.com/bypassing-cors-with-electron-ab7eaf331605
 function UpsertKeyValue(obj, keyToChange, value) {
-	const keyToChangeLower = keyToChange.toLowerCase();
-	for (const key of Object.keys(obj)) {
-		if (key.toLowerCase() === keyToChangeLower) {
-			// Reassign old key
-			obj[key] = value;
-			// Done
-			return;
-		}
-	}
-	// Insert at end instead
-	obj[keyToChange] = value;
+  const keyToChangeLower = keyToChange.toLowerCase();
+  for (const key of Object.keys(obj)) {
+    if (key.toLowerCase() === keyToChangeLower) {
+      // Reassign old key
+      obj[key] = value;
+      // Done
+      return;
+    }
+  }
+  // Insert at end instead
+  obj[keyToChange] = value;
 }
 
 function appOver18() {
-	over18 = !over18;
-	mainWindow.webContents.executeJavaScript(`localStorage.setItem('over18', ${over18})`, true);
+  over18 = !over18;
+  mainWindow.webContents.executeJavaScript(`localStorage.setItem('over18', ${over18})`, true);
 }
