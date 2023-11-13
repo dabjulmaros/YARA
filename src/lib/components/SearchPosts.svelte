000d5edb272fa3baf9a028f3d427a98f46a6cb01
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-11-13 06:52:29 -0500

    added styles to search

--- src/lib/components/SearchPosts.svelte
+++ src/lib/components/SearchPosts.svelte
@@ -8,16 +8,35 @@
 <div>
 	{#each items as data}
 		<!-- filters out users -->
-		{#if data.title !== ''}
+		{#if data.title !== '' && data.members !== ''}
 			<article>
 				<div class="body">
 					<h2>
 						<AncherNoreferrer link={`${data.link}`} content={`View ${data.title}`} />
 					</h2>
-					<small>{shortNum(null, data.members)}</small>
+					<small>{shortNum(null, data.members)} subscribers</small>
 					<p>{data.description}</p>
 				</div>
 			</article>
 		{/if}
 	{/each}
 </div>
+
+<style>
+	article {
+		max-width: 1500px;
+		margin: 1rem auto;
+	}
+	div.body {
+		display: flex;
+		flex-direction: column;
+	}
+	h2 {
+		margin-bottom: 0.5rem;
+	}
+	@media (min-width: 801px) {
+		div.body {
+			min-height: 5vh;
+		}
+	}
+</style>
