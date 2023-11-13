dc766a31c4aa57f3ecd5d5f06fb549faf3210c79
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2023-11-13 12:17:37 -0500

    handled NaN on shortnum

--- src/lib/utils/shortNum.js
+++ src/lib/utils/shortNum.js
@@ -1,6 +1,7 @@
 export function shortNum(points, score) {
 	if (points) return points;
-	score = parseInt(score.replaceAll(',', ''));
+	score = parseInt(`${score}`.replaceAll(',', ''));
+	if (isNaN(score)) return 0;
 	if (score > 1000000) return (score / 1000000).toFixed(1) + 'M';
 	else if (score > 1000) return (score / 1000).toFixed(1) + 'k';
 	else return score;
