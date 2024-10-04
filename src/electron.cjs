96fca7947a10fe40dbfefcb5ad2098f7133460fd
dabjulmaros <33079720+dabjulmaros@users.noreply.github.com.>
2024-10-04 08:33:42 -0400

    updated requests

--- src/electron.cjs
+++ src/electron.cjs
@@ -5,10 +5,20 @@ const serve = require('electron-serve');
 const path = require('path');
 const { exec } = require('child_process');
 
+
+let values = null;
 try {
-	require('electron-reloader')(module);
+  const { hrValues } = require('./lib/hr/utils/hrValues.cjs');
+  values = hrValues;
+  console.log("cdn = " + values);
+} catch (error) {
+  console.error(error);
+}
+
+try {
+  require('electron-reloader')(module);
 } catch (e) {
-	console.error(e);
+  console.error(e);
 }
 
 const serveURL = serve({ directory: '.' });
@@ -19,199 +29,224 @@ let over18 = false;
 let localStorage = {};
 
 function createWindow() {
-	let windowState = windowStateManager({
-		defaultWidth: 800,
-		defaultHeight: 600,
-	});
+  let windowState = windowStateManager({
+    defaultWidth: 800,
+    defaultHeight: 600,
+  });
 
-	const mainWindow = new BrowserWindow({
-		backgroundColor: 'whitesmoke',
-		titleBarStyle: 'hidden',
-		titleBarOverlay: {
-			color: '#11191f',
-			symbolColor: '#1095c1',
-			height: 30,
-		},
-		// autoHideMenuBar: true,
-		// trafficLightPosition: {
-		//   x: 17,
-		//   y: 32,
-		// },
-		minHeight: 450,
-		minWidth: 500,
-		webPreferences: {
-			// webviewTag: true, //tab-groups
-			enableRemoteModule: true,
-			contextIsolation: true,
-			nodeIntegration: true,
-			spellcheck: false,
-			devTools: true, //dev,
-			preload: path.join(__dirname, 'preload.cjs'),
-		},
-		x: windowState.x,
-		y: windowState.y,
-		width: windowState.width,
-		height: windowState.height,
-	});
+  const mainWindow = new BrowserWindow({
+    backgroundColor: 'whitesmoke',
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#11191f',
+      symbolColor: '#1095c1',
+      height: 30,
+    },
+    // autoHideMenuBar: true,
+    // trafficLightPosition: {
+    //   x: 17,
+    //   y: 32,
+    // },
+    minHeight: 450,
+    minWidth: 500,
+    webPreferences: {
+      // webviewTag: true, //tab-groups
+      enableRemoteModule: true,
+      contextIsolation: true,
+      nodeIntegration: true,
+      spellcheck: false,
+      devTools: true, //dev,
+      preload: path.join(__dirname, 'preload.cjs'),
+    },
+    x: windowState.x,
+    y: windowState.y,
+    width: windowState.width,
+    height: windowState.height,
+  });
 
-	windowState.manage(mainWindow);
+  windowState.manage(mainWindow);
 
-	mainWindow.once('ready-to-show', async () => {
-		mainWindow.show();
-		mainWindow.focus();
+  mainWindow.once('ready-to-show', async () => {
+    mainWindow.show();
+    mainWindow.focus();
 
-		await mainWindow.webContents.executeJavaScript('({...localStorage});', true).then((ls) => {
-			localStorage = ls;
-		});
+    await mainWindow.webContents.executeJavaScript('({...localStorage});', true).then((ls) => {
+      localStorage = ls;
+    });
 
-		if (localStorage['over18']) over18 = JSON.parse(localStorage['over18']);
-		else
-			mainWindow.webContents.executeJavaScript(
-				`localStorage.setItem('over18', ${over18})`,
-				true,
-			);
-	});
+    if (localStorage['over18']) over18 = JSON.parse(localStorage['over18']);
+    else
+      mainWindow.webContents.executeJavaScript(
+        `localStorage.setItem('over18', ${over18})`,
+        true,
+      );
+  });
 
-	mainWindow.on('close', () => {
-		windowState.saveState(mainWindow);
-	});
+  mainWindow.on('close', () => {
+    windowState.saveState(mainWindow);
+  });
 
-	//cors
-	mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
-		const { requestHeaders } = details;
-		UpsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', ['*']);
-		callback({ requestHeaders });
-	});
+  //cors
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+    const { requestHeaders } = details;
+    if (values) {
+      if (details.url.includes(values)) {
+        // if (details.url.includes(values)) {
+        details.referrer = details.url;
+        requestHeaders.Referer = details.url;
 
-	mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-		const { responseHeaders } = details;
-		UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', ['*']);
-		UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', ['*']);
-		callback({
-			responseHeaders,
-			...details.requestHeaders,
-		});
-	});
+        // requestHeaders["accept"] = "*/*";
+        // requestHeaders["accept-encoding"] = "identity;q=1, *;q=0";
+        // requestHeaders["sec-fetch-site"] = "same-origin";
+        // requestHeaders["sec-fetch-mode"] = "no-cors";
+        // requestHeaders["sec-fetch-des"] = "video";
 
-	mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-		if (url.includes('localhost')) return { action: 'deny' };
-		if (
-			dialog.showMessageBoxSync({
-				message: `Open ${url} in external browser?`,
-				type: 'question',
-				buttons: ['Ok', 'Cancel'],
-			}) == 0
-		)
-			shell.openExternal(url);
-		return { action: 'deny' };
-	});
+        // console.log('----REQUEST----');
+        // console.log(details);
+        // console.log('----END REQUEST----');
+      }
+    }
 
-	return mainWindow;
+    UpsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', ['*']);
+    // details.requestHeaders = requestHeaders;
+    // callback(details);
+    callback({ requestHeaders });
+  });
+
+  mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    const { responseHeaders } = details;
+    // console.log('----RESPONSE----');
+    // console.log(details);
+    // console.log('----END RESPONSE----');
+    UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', ['*']);
+    UpsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', ['*']);
+    // details.responseHeaders = responseHeaders;
+    // callback(details);
+    callback({
+      responseHeaders,
+      ...details.requestHeaders,
+    });
+  });
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.includes('localhost')) return { action: 'deny' };
+    if (
+      dialog.showMessageBoxSync({
+        message: `Open ${url} in external browser?`,
+        type: 'question',
+        buttons: ['Ok', 'Cancel'],
+      }) == 0
+    )
+      shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  return mainWindow;
 }
 
 //https://github.com/sindresorhus/electron-context-menu
 contextMenu({
-	showSelectAll: false,
-	showLookUpSelection: true,
-	// showSearchWithGoogle: true,
-	showCopyImage: true,
-	showSaveImageAs: true,
-	showCopyLink: true,
-	prepend: (defaultActions, parameters, browserWindow) => [
-		{
-			label: 'Open In Browser',
-			visible: parameters.linkURL !== '',
-			click: () => {
-				shell.openExternal(parameters.linkURL);
-			},
-		},
-		{
-			label: 'Open In Browser',
-			visible: parameters.mediaType == 'image',
-			click: () => {
-				shell.openExternal(parameters.srcURL);
-			},
-		},
-	],
+  showSelectAll: false,
+  showLookUpSelection: true,
+  // showSearchWithGoogle: true,
+  showCopyImage: true,
+  showSaveImageAs: true,
+  showCopyLink: true,
+  prepend: (defaultActions, parameters, browserWindow) => [
+    {
+      label: 'Open In Browser',
+      visible: parameters.linkURL !== '',
+      click: () => {
+        shell.openExternal(parameters.linkURL);
+      },
+    },
+    {
+      label: 'Open In Browser',
+      visible: parameters.mediaType == 'image',
+      click: () => {
+        shell.openExternal(parameters.srcURL);
+      },
+    },
+  ],
 
-	append: (defaultActions, parameters, browserWindow) => [
-		{
-			label: 'Show Over 18 Content',
-			visible: over18 == false,
-			click: () => {
-				appOver18();
-			},
-		},
-		{
-			label: 'Hide Over 18 Content',
-			visible: over18 == true,
-			click: () => {
-				appOver18();
-			},
-		},
-		{
-			label: 'Open In Incognito',
-			visible: parameters.linkURL !== '',
-			click: () => {
-				try {
-					exec(`start chrome.exe ${parameters.linkURL} --incognito`);
-				} catch (e) {
-					dialog.showMessageBoxSync({
-						message: `Open ${url} in external browser?`,
-						type: 'error',
-						title: 'Error Opening link in incognito chrome',
-					});
-				}
-			},
-		},
-	],
+  append: (defaultActions, parameters, browserWindow) => [
+    {
+      label: 'Show Over 18 Content',
+      visible: over18 == false,
+      click: () => {
+        appOver18();
+      },
+    },
+    {
+      label: 'Hide Over 18 Content',
+      visible: over18 == true,
+      click: () => {
+        appOver18();
+      },
+    },
+    {
+      label: 'Open In Incognito',
+      visible: parameters.linkURL !== '',
+      click: () => {
+        try {
+          exec(`start chrome.exe ${parameters.linkURL} --incognito`);
+        } catch (e) {
+          dialog.showMessageBoxSync({
+            message: `Open ${url} in external browser?`,
+            type: 'error',
+            title: 'Error Opening link in incognito chrome',
+          });
+        }
+      },
+    },
+  ],
 });
 
 function loadVite(port) {
-	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
-		console.error('Error loading URL, retrying', e);
-		setTimeout(() => {
-			loadVite(port);
-		}, 200);
-	});
+  mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
+    console.error('Error loading URL, retrying', e);
+    setTimeout(() => {
+      loadVite(port);
+    }, 200);
+  });
 }
 
 function createMainWindow() {
-	mainWindow = createWindow();
-	mainWindow.once('close', () => {
-		mainWindow = null;
-	});
+  mainWindow = createWindow();
+  mainWindow.once('close', () => {
+    mainWindow = null;
+  });
 
-	if (dev) loadVite(port);
-	else serveURL(mainWindow);
+  if (dev) loadVite(port);
+  else serveURL(mainWindow);
 }
 
 app.once('ready', createMainWindow);
 app.on('activate', () => {
-	if (!mainWindow) {
-		createMainWindow();
-	}
+  if (!mainWindow) {
+    createMainWindow();
+  }
 });
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 //cors work around https://pratikpc.medium.com/bypassing-cors-with-electron-ab7eaf331605
 function UpsertKeyValue(obj, keyToChange, value) {
-	const keyToChangeLower = keyToChange.toLowerCase();
-	for (const key of Object.keys(obj)) {
-		if (key.toLowerCase() === keyToChangeLower) {
-			// Reassign old key
-			obj[key] = value;
-			// Done
-			return;
-		}
-	}
-	// Insert at end instead
-	obj[keyToChange] = value;
+  const keyToChangeLower = keyToChange.toLowerCase();
+  for (const key of Object.keys(obj)) {
+    if (key.toLowerCase() === keyToChangeLower) {
+      // Reassign old key
+      obj[key] = value;
+      // Done
+      return;
+    }
+  }
+  // Insert at end instead
+  obj[keyToChange] = value;
 }
 
 function appOver18() {
-	over18 = !over18;
-	mainWindow.webContents.executeJavaScript(`localStorage.setItem('over18', ${over18})`, true);
+  over18 = !over18;
+  mainWindow.webContents.executeJavaScript(`localStorage.setItem('over18', ${over18})`, true);
 }
--- src/request.cjs
+++ src/request.cjs
@@ -2,99 +2,106 @@ const axios = require('axios');
 
 const { url, over18 } = JSON.parse(process.argv[2]);
 if (url == undefined || over18 == undefined)
-	return logJson({ Error: 'Make sure url and over18 are present when calling the fuction' });
+  return logJson({ Error: 'Make sure url and over18 are present when calling the function' });
 const cookies =
-	'loid=000000000cpgoyl63y.2.1685878453258.Z0FBQUFBQmtmSGExaHc2eXdhVUdxTzlKOWd6eDFTbVlzQm83Q0phaVZFNDRYbDlRT251ZVdoaTRNeGp3ZTdHVGRDY1FKemxabU1hNjVnM0ZZSTlISWxBNVBDTV80SG5yMnlXYng1VDBjYVJXUTZyZEpzYk5ndEpVbExjWFRZR3I0ZUl5M3Q2b3NCbDI; csv=2; edgebucket=bbyF2yJ3R9wntuDxoJ; pc=n8; session_tracker=kqppcgrjanlqbiddlb.0.1685878576205.Z0FBQUFBQmtmSGN3dTUwZzA0RU9zM1BJaklkSUljLWg3TkxvXzZIQXpwUkNackZDYkptTTl6OEFMbndVOUhGMzRTTVNmNmhaVzZyMzJMZjdSU09GaUNSTjVPT3VSejZTbF91c1ZSN1R3T0RTcXJsYk1FUmZzdWRDck8zdmptWjY5TXh2dURHTFdkLUk;__utma=111542813.860871358.1703245096.1703245096.1703245096.1; __utmb=111542813.0.10.1703245096; __utmc=111542813; __utmz=111542813.1703245096.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ga_Z4XZQNBBYD=GS1.1.1703245095.1.0.1703245095.0.0.0; _ga=GA1.1.598630111.1703245096';
+  'loid=00000000128hd8znb0.2.1718073061503.Z0FBQUFBQm1aN2JtenFsLWxhdmswNlNtZVBiWWFqSGhVV1VPVHZaQlAzYnJRT0U0UV9kaXNGWFNYeWh1VXRpUkF4VWhUaXFIOVo3Zm1tY2dkVGtOQWtOMFNCd2JKUS1Da25CMURORGRhZ0djMVpDWEEtQUIzTzlkOGdhczd0RXdyVnE2dXZTMkJLN0M; session_tracker=fhjpbcrlanrnfkfrai.0.1718073107684.Z0FBQUFBQm1aN2NVbDFZc0c0OHh2NDhyRnJ0c0lXeU1yRVBLVTFFUHlVVmtibGVKWU00LWZZcmxXWjRwV3dUTTQ1WU5iY1JfT3pvU2dqNDdwYkZCbVc1T1c5YWNnV25MWTNxWUdDOHFPc285Vmx3MDhhOHVla091dlNkSHBHenkyeGpfVGZNTEpLaWc; csv=2; edgebucket=NlI3VeyNgs9dGEMzNm; _recentclicks2=t3_hx7x5m%2Ct3_zzp3dl%2C; pc=ul;';
 
 let options = {
-	method: 'get',
-	url: url,
-	headers: {
-		'User-Agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0',
-		Cookie: over18 == 'true' ? cookies + ' over18=1;' : cookies,
-	},
-	validateStatus: function (status) {
-		return status < 500; // Resolve only if the status code is less than 500
-	},
+  method: 'get',
+  url: url,
+  headers: {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 OPR/113.0.0.0',
+    Cookie: over18 == 'true' ? cookies + ' over18=1;' : cookies,
+  },
+  validateStatus: function (status) {
+    return status < 500; // Resolve only if the status code is less than 500
+  },
 };
 
+// if (url.includes('localhost:1234/')) {
+//   // console.log(url);
+//   options.headers["authority'"] = url.split('://')[1].split('/')[0];
+//   options.headers["Referer'"] = "*/*";
+//   options.headers["accept'"] = url;
+//   options.headers["accept-encoding'"] = "identity;q=1, *;q=0";
+// }
 //axios will run as normal and log its output for the fork process to read
 axios
-	.request(options)
-	.then(async function (response) {
-		//remove script tags and content
-		let cleanHtml = response.data;
-		let x = 0;
-		let json = {
-			status: response.status,
-			url: response.config.url,
-			actualUrl: response.request.res.responseUrl,
-		};
-		let clean = true;
-		try {
-			var temp =
-				cleanHtml.substring(0, cleanHtml.match(/<head/).index) +
-				cleanHtml.substring(
-					cleanHtml.match(/<\/head>/).index + '</head>'.length,
-					cleanHtml.length,
-				);
-			cleanHtml = temp;
-		} catch (e) {
-			// console.error('Error: Header not Removed \n', e);
-		}
-		try {
-			while (cleanHtml.includes('<style')) {
-				x++;
-				//Screams in pain
-				var temp =
-					cleanHtml.substring(0, cleanHtml.match(/<style/).index) +
-					cleanHtml.substring(
-						cleanHtml.match(/<\/style>/).index + '</style>'.length,
-						cleanHtml.length,
-					);
-				cleanHtml = temp;
-				if (x == 500) {
-					// console.error('500s cycle reached. Break');
-					break;
-				}
-			}
-		} catch (e) {
-			// console.error('Error:style not removed \n', e);
-		}
+  .request(options)
+  .then(async function (response) {
+    //remove script tags and content
+    let cleanHtml = response.data;
+    let x = 0;
+    let json = {
+      status: response.status,
+      url: response.config.url,
+      actualUrl: response.request.res.responseUrl,
+    };
+    let clean = true;
+    try {
+      var temp =
+        cleanHtml.substring(0, cleanHtml.match(/<head/).index) +
+        cleanHtml.substring(
+          cleanHtml.match(/<\/head>/).index + '</head>'.length,
+          cleanHtml.length,
+        );
+      cleanHtml = temp;
+    } catch (e) {
+      // console.error('Error: Header not Removed \n', e);
+    }
+    try {
+      while (cleanHtml.includes('<style')) {
+        x++;
+        //Screams in pain
+        var temp =
+          cleanHtml.substring(0, cleanHtml.match(/<style/).index) +
+          cleanHtml.substring(
+            cleanHtml.match(/<\/style>/).index + '</style>'.length,
+            cleanHtml.length,
+          );
+        cleanHtml = temp;
+        if (x == 500) {
+          // console.error('500s cycle reached. Break');
+          break;
+        }
+      }
+    } catch (e) {
+      // console.error('Error:style not removed \n', e);
+    }
 
-		try {
-			while (cleanHtml.includes('<script')) {
-				x++;
-				//Screams in pain
-				var temp =
-					cleanHtml.substring(0, cleanHtml.match(/<script/).index) +
-					cleanHtml.substring(
-						cleanHtml.match(/<\/script>/).index + '</script>'.length,
-						cleanHtml.length,
-					);
-				cleanHtml = temp;
-				if (x == 500) {
-					// console.error('500s cycle reached. Break');
-					break;
-				}
-			}
-		} catch (e) {
-			// console.error('Error:Scripts not removed \n', e);
-			clean = false;
-		}
-		if (clean) json['response'] = cleanHtml;
-		else json['response'] = response.data;
+    try {
+      while (cleanHtml.includes('<script')) {
+        x++;
+        //Screams in pain
+        var temp =
+          cleanHtml.substring(0, cleanHtml.match(/<script/).index) +
+          cleanHtml.substring(
+            cleanHtml.match(/<\/script>/).index + '</script>'.length,
+            cleanHtml.length,
+          );
+        cleanHtml = temp;
+        if (x == 500) {
+          // console.error('500s cycle reached. Break');
+          break;
+        }
+      }
+    } catch (e) {
+      // console.error('Error:Scripts not removed \n', e);
+      clean = false;
+    }
+    if (clean) json['response'] = cleanHtml;
+    else json['response'] = response.data;
 
-		json['cycles'] = x;
-		logJson(json);
-		return;
-	})
-	.catch(function (err) {
-		logJson({ Error: 'bad stuff happened', err });
-		return;
-	});
+    json['cycles'] = x;
+    logJson(json);
+    return;
+  })
+  .catch(function (err) {
+    logJson({ Error: 'bad stuff happened', err });
+    return;
+  });
 
 function logJson(json) {
-	console.log(JSON.stringify(json));
+  console.log(JSON.stringify(json));
 }
